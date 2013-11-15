80d026ec55f290bd03974970d02e4623d4c56d03
My Nguyen <salazarsx@gmail.com>
2013-11-15 23:52:47 +0700

    DieRoll

--- FineTeam_jdice/src/DieRoll.java
+++ FineTeam_jdice/src/DieRoll.java
@@ -42,7 +42,7 @@ public class DieRoll {
 	    int roll=rnd.nextInt(nsides)+1;
 	    r.addResult(roll); //"." >","
 	}
-	return;  //returnr>return
+	return r;  //returnr>return r
     }
     public String toString() {
 	String ans =ndice+"d"+nsides;
